f4a5eeda924fa515b4aeee915d8feb0c5c11970a
Ashish-1052 <akedari1052@gmail.com>
2025-12-26 17:10:47 +0530

    add pokemon list and page

--- index.js
+++ index.js
@@ -4,10 +4,39 @@ import axios from "axios";
 const app = express();
 const port = 3000;
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`)
 })
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+app.get("/", async (req, res) => {
+  try {
+    const url = req.query.pokemonPageUrl;
+    if (url) {
+      const result = await axios.get(url);
+      res.render("index.ejs", { pokemon: result.data.results, next: result.data.next, previous: result.data.previous });
+      return;
+    }
+    const result = await axios.get("https://pokeapi.co/api/v2/pokemon?limit=20&offset=0");
+    res.render("index.ejs", { pokemon: result.data.results, next: result.data.next, previous: result.data.previous });
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+app.get("/pokemon", async (req, res) => {
+  try {
+    if (req.query.name) {
+      const result = await axios.get(`https://pokeapi.co/api/v2/pokemon/${req.query.name}`);
+      res.render("pokemon.ejs", { pokemon: result.data });
+      return;
+    }
+    const url = req.query.pokemonUrl;
+    const result = await axios.get(url);
+    res.render("pokemon.ejs", { pokemon: result.data });
+  } catch (error) {
+    console.log(error);
+  }
 });
